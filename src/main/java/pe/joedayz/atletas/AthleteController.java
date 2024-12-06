2dc7e3a6157793539b915985be7486dcb3e0632a
Jose Amadeo Diaz Diaz <jose.diaz@joedayz.pe>
2024-12-05 20:32:46 -0500

    init binder - pre process antes de los RequestMapping

--- src/main/java/pe/joedayz/atletas/AthleteController.java
+++ src/main/java/pe/joedayz/atletas/AthleteController.java
@@ -1,10 +1,12 @@
 package pe.joedayz.atletas;
 
 import jakarta.validation.Valid;
-import org.springframework.context.annotation.Configuration;
+import org.springframework.beans.propertyeditors.StringTrimmerEditor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.WebDataBinder;
+import org.springframework.web.bind.annotation.InitBinder;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -15,17 +17,26 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @RequestMapping("/player")
 public class AthleteController {
 
+  @InitBinder
+  public void initBinder(WebDataBinder binder) {
+    StringTrimmerEditor stringTrimmerEditor = new StringTrimmerEditor(true);
+    binder.registerCustomEditor(String.class, stringTrimmerEditor);
+    //binder.addValidators()
+    //binder.addCustomFormatter()
+  }
 
   @RequestMapping("/showPlayerForm")
-  public String showForm(Model model){
+  public String showForm(Model model) {
     model.addAttribute("athlete", new Athlete());
     return "add-player-form";
   }
+
   @RequestMapping("/processPlayerForm")
-  public String processForm(@Valid @ModelAttribute("athlete") Athlete athlete, BindingResult result){
-    if(result.hasErrors()){
+  public String processForm(@Valid @ModelAttribute("athlete") Athlete athlete,
+      BindingResult result) {
+    if (result.hasErrors()) {
       return "add-player-form";
-    }else{
+    } else {
       return "player-confirmation";
     }
   }
